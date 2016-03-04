5524dcbb8073587580c5e365dbfa129c791957f0
sigurjonrv <sigurjonv12@ru.is>
2016-03-04 16:43:51 +0000

    view fyrir sellers

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -5,4 +5,12 @@ function SellersController($scope, AppResource) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
 
+	var sellerListPromise = AppResource.getSellers();
+	
+	sellerListPromise.success(function(sellersList){
+		console.log(sellersList);
+		$scope.sellers = sellersList;
+	});
+
+
 });
