bbb2db127fe0cdb176835f2a88d0cc81dac145e7
sigurjonrv <sigurjonv12@ru.is>
2016-03-11 16:02:18 +0000

    test

--- src/components/sellers/SellersController.spec.js
+++ src/components/sellers/SellersController.spec.js
@@ -1,5 +1,30 @@
 "use strict";
 
-describe("SellersController should be unit tested here", function() {
-	// TODO: add beforeEach/describe/it/etc. functions as appropriate!
+describe("SellersController", function() {
+
+	// Load the controllers module
+	beforeEach(module("project3App"));
+
+	var scope, resource, SellersController;
+
+	var mockLocation = {
+		path: function(p) {
+		}
+	};
+
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function($rootScope, AppResource, $controller) {
+		scope = $rootScope.$new();
+		resource = AppResource;
+		spyOn(mockLocation, "path");
+		SellersController = $controller('SellersController', {
+			$scope: scope,
+			AppResource: resource,
+			$location: mockLocation
+		});
+	}));
+
+	it('addseller should be defined', function() {
+		expect(scope.addSeller).toBeDefined;
+	});
 });
